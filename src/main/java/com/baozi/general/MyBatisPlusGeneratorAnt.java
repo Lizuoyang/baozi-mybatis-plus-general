a19242891aac8bec0b6a91a667156cea2f98fdb0
lizuoyang <lizuoyangsina@qq.com>
2021-04-09 10:53:53 +0800

    feat: 自定义摸板

--- src/main/java/com/baozi/general/MyBatisPlusGeneratorAnt.java
+++ src/main/java/com/baozi/general/MyBatisPlusGeneratorAnt.java
@@ -172,7 +172,7 @@ public class MyBatisPlusGeneratorAnt {
 
         // 配置自定义输出模板
         //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
-        // templateConfig.setEntity("templates/entity2.java");
+        templateConfig.setEntity("templates/entity.java");
         // templateConfig.setService();
         // templateConfig.setController();
 
